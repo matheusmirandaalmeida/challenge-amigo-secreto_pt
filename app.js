891c5b1492decc5fef053de63c6b1c3d118422a8
Matheus M Almeida <matheus.mma2015@gmail.com>
2025-02-04 13:41:51 -0300

    projeto concluído

--- app.js
+++ app.js
@@ -1,1 +1,59 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo(){
+    let input = document.getElementById('amigo');
+    const nomeAmigo = input.value.trim();
+
+    console.log(nomeAmigo);
+
+    if(nomeAmigo === ''){
+        alert('Por favor preencher campo nome.');
+        return;
+    }
+
+    amigos.push(nomeAmigo);
+
+    atualizarListaAmigos();
+
+    input.value = '';
+    input.focus();
+}
+
+function atualizarListaAmigos(){
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    amigos.forEach((amigo) => {
+        const novoItem = document.createElement('li');
+        novoItem.textContent = amigo;
+        listaAmigos.appendChild(novoItem);
+    });
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert('Adicionar pelo menos um nome antes de sortear.');
+        return
+    }
+
+    const indiceSorteado = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceSorteado];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>Amigo sorteado: <strong>${amigoSorteado}</strong></li>`;
+}
+
+function resetar(){
+    amigos = [];
+
+    const listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = '';
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = '';
+
+    document.getElementById('amigo').focus();
+}
